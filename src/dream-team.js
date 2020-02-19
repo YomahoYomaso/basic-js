7d88ab248e7aa1c925f9ac900fcca11d915d3e13
Dani <barborossa707@gmail.com>
2020-02-19 21:02:06 +0300

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    var name = "";
+    // remove line with error and write your code here
+    try {
+        for (var i = 0; i < members.length; i++) {
+            if (typeof members[i] === "string") {
+              members[i] = members[i].split(" ").join("");
+              let firstChar = members[i].split("");
+              name = name + firstChar[0];
+            }
+
+        }
+        name = name.toUpperCase().split("").sort().join("");
+        console.log(name);
+        return name;
+    } catch (e) {
+        return false;
+    }
 };
